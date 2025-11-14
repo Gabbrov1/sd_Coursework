b555719ca5bc09e7ba9028cc77e4b8d1aaa247b3
Gabbrov1 <ionut4965@gmail.com>
2025-11-14 11:01:57 +0000

    Basic setup
    created the bare minimum, which includes:
    
    Basic API
    Basic FrontEnd
    
    Enabled CORS for frontend

---
+++ backend/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+app = Flask(__name__)
+
+#=======================================================================================
+# Specific route with CORS enabled example. DELETE
+#@app.route("/api/data")
+#@cross_origin(origin="http://localhost:4321")
+#def data():
+#    return {"msg": "Hello"}
+#=======================================================================================
+
+
+# Enable Cross-Origin Resource Sharing (CORS) for the specified origin
+CORS(app,resources={r"/*": {"origins": "http://localhost:4321"}})
+
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    return jsonify({"message": "Hello, Gab!"})
+
+@app.route('/api/GET', methods=['GET'])
+def GET():
+    return jsonify({"message": "Hello, World!"})
+
+@app.route('/api/POST', methods=['POST'])
+def POST():
+    return (temporary_debug,200)
+
+def temporary_debug():
+    return "This is a temporary debug route."
+
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
