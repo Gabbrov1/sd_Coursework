f5697a51c6058d59ea651e7ea14c8d5846886f1c
Gabbrov1 <ionut4965@gmail.com>
2026-01-27 02:48:16 +0000

    implemented Mongo

--- backend/HelperFunctions/Database.py
+++ backend/HelperFunctions/Database.py
@@ -69,7 +69,7 @@ def getGamesByPage(page = 0, rows_per_page=10):
 
 #NoSQL changed from Firestore to Mongo DB due to cost.
 
-client = MongoClient(f"{os.getenv('MongoServer','mongodb://localhost:27017')}")
+client = MongoClient(f"{os.getenv('MongoServer','mongodb://localhost')}:{os.getenv('MongoPort','27017')}")
 MongoDb = client[f"{os.getenv('MongoDb','SysDevComments')}"]  
 
 usersCol = MongoDb["users"]
