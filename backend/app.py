69237f7b5929e854c830eb9bd1463ce32c884770
Gabbrov1 <ionut4965@gmail.com>
2026-01-27 08:13:01 +0000

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -31,9 +31,7 @@ google = oauth.register(
     name="google",
     client_id=os.getenv("Google_Client"),
     client_secret=os.getenv("Google_Secret"),
-    access_token_url="https://oauth2.googleapis.com/token",
-    authorize_url="https://accounts.google.com/o/oauth2/auth",
-    api_base_url="https://www.googleapis.com/oauth2/v2/",
+    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
     client_kwargs={
         "scope": "openid email profile"
     }
